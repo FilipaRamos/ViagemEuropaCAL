376ea086cdc3d0b5a5adaebc4bfa81fcc85370da
FilipaRamos <fimamora@gmail.com>
2015-04-22 01:19:31 +0100

    sem erros mas crasha ao correr

---
+++ src/ViagemEuropaCAL.cpp
@@ -0,0 +1,21 @@
+//============================================================================
+// Name        : ViagemEuropaCAL.cpp
+// Author      :
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+#include "Files.h"
+#include "Cidade.h"
+#include "Viagem.h"
+
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
+
+
