b711fb66a99e4521b288fb834dff9fc7d2bcd7d2
marianagoliveira <mariana.oliveira@sapo.pt>
2015-04-23 14:29:46 +0100

    Ja tenho a socket adicionada , make sure you do too

--- src/DisplayGraph.cpp
+++ src/DisplayGraph.cpp
@@ -6,10 +6,17 @@
  */
 
 #include "DisplayGraph.h"
-
+#include "graphviewer.h"
 
 void DisplayGraph::Display() {
 	// TODO Auto-generated constructor stub
+	GraphViewer *gv = new GraphViewer(600, 600, true);
+
+	gv->createWindow(600, 600);
+
+	gv->defineVertexColor(BLUE);
+
+	gv->defineEdgeColor(BLACK);
 
 }
 
