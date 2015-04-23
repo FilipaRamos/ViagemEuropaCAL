66bb2656a09aeaf3657b790aec2699fc12ae0192
marianagoliveira <mariana.oliveira@sapo.pt>
2015-04-23 14:13:52 +0100

    Visualizador adicionado

--- src/DisplayGraph.cpp
+++ src/DisplayGraph.cpp
@@ -6,21 +6,11 @@
  */
 
 #include "DisplayGraph.h"
-#include "graphviewer.h"
 
-DisplayGraph::DisplayGraph() {
+
+void DisplayGraph::Display() {
 	// TODO Auto-generated constructor stub
-	GraphViewer *gv = new GraphViewer(600, 600, true);
-
-	gv->createWindow(600, 600);
-
-	gv->defineVertexColor("blue");
-
-	gv->defineEdgeColor("black");
 
 }
 
-DisplayGraph::~DisplayGraph() {
-	// TODO Auto-generated destructor stub
-}
 
