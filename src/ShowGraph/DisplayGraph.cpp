b690a47803485dee43388e9ac674eb1b330b25c9
marianagoliveira <mariana.oliveira@sapo.pt>
2015-04-25 23:32:57 +0100

    so faz o display do primeiro nó

--- src/ShowGraph/DisplayGraph.cpp
+++ src/ShowGraph/DisplayGraph.cpp
@@ -22,15 +22,16 @@ void DisplayGraph::Display(Graph<Cidade> gc) {
 	//ADICIONA OS NOS TODOS
 
 	for (size_t i = 0; i < gc.getVertexSet().size(); ++i) {
+		while (i < gc.getVertexSet().at(i)->getInfo().getTemposViagem().size()) {
 		gv->addNode(gc.getVertexSet().at(i)->getInfo().getTemposViagem().at(i));
-
+		}
 	}
 
 	for (size_t i = 1; i <= gc.getVertexSet().size(); ++i) {
 		unsigned int j = 0;
-		while (j < gc.getVertexSet().at(i-1)->getInfo().getTemposViagem().size()) {
+		while (j < gc.getVertexSet().at(i)->getInfo().getTemposViagem().size()) {
 			gv->addEdge(gc.getVertexSet().at(i)->getInfo().getClassificacao(), gc.getVertexSet().at(i-1)->getInfo().getClassificacao(), gc.getVertexSet().at(i)->getInfo().getTemposViagem()[j], EdgeType::UNDIRECTED);
-			//      gv->addEdge(gc.getVertexSet().at(i)->getInfo().getClassificacao(), gc.getVertexSet().at(i-1)->getInfo().getClassificacao(), gc.getVertexSet().at(i)->getInfo().getTemposViagem()[j], EdgeType::UNDIRECTED);
+			gv->addEdge(gc.getVertexSet().at(i)->getInfo().getClassificacao(), gc.getVertexSet().at(i-1)->getInfo().getClassificacao(), gc.getVertexSet().at(i)->getInfo().getTemposViagem()[j], EdgeType::UNDIRECTED);
 
 			++j;
 		}
