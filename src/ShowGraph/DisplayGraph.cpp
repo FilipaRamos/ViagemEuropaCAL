1a7594f1021b97a1eacb11d8d33a32064dc1be43
FilipaRamos <fimamora@gmail.com>
2015-04-26 10:42:32 +0100

    after merge commit --> fixed

--- src/ShowGraph/DisplayGraph.cpp
+++ src/ShowGraph/DisplayGraph.cpp
@@ -7,20 +7,6 @@
 DisplayGraph::DisplayGraph(){
 }
 
-<<<<<<< HEAD
-=======
-int DisplayGraph::StringInt(string st) {
-
-	std::istringstream ss(st);
-	int i;
-	ss >> i;
-	std::cout << i;
-
-	return i;
-
-}
-
->>>>>>> origin/master
 void DisplayGraph::Display(Graph<Cidade> gc) {
 
 	GraphViewer *gv = new GraphViewer(600, 600, true);
