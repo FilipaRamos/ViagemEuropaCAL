40476b5f102a85a18e1af1d7568858aaf659d8d3
FilipaRamos <fimamora@gmail.com>
2015-04-27 03:40:21 +0100

    The end

--- src/ Knapsackproblem.cpp
+++ src/ Knapsackproblem.cpp
@@ -19,50 +19,54 @@ using namespace std;
  * Retorna um vector com as Cidades a serem usadas na construção do grafo.
  *
  */
-vector<Cidade> Knapsackproblem(Viagem v, Path p, int tempoMax){
+vector<Cidade> Knapsackproblem(Viagem v, Path p, int tempoMax) {
 
 	vector<Cidade> res;
 	//int a[v.getCidades().size()+1][tempoMax+1];
 
-	p.getGraph().setA(new int *[v.getCidades().size()+1]);
+	p.getGraph().setA(new int *[v.getCidades().size() + 1]);
 
-	for(size_t i=0; i < v.getCidades().size()+1; ++i){
-		p.getGraph().getA()[i] = new int [tempoMax+1];
+	for (size_t i = 0; i < v.getCidades().size() + 1; ++i) {
+		p.getGraph().getA()[i] = new int[tempoMax + 1];
 	}
 
 	//INICIALIZAR TUDO A ZERO
-	for(size_t i=0; i < v.getCidades().size()+1; ++i){
-		for(size_t j=0; j < tempoMax +1; ++j){
+	for (size_t i = 0; i < v.getCidades().size() + 1; ++i) {
+		for (size_t j = 0; j < tempoMax + 1; ++j) {
 			p.getGraph().getA()[i][j] = 0;
 		}
 	}
 
 	int iMax = 0;
 	int jMax = 0;
-	vector<pair<int,int> > classificacoes;
+	vector<pair<int, int> > classificacoes;
 
-	for(size_t i =0; i < v.getCidades().size(); ++i){
-		for(size_t j=0; j < tempoMax; ++j){
-			if(i==0){
+	for (size_t i = 0; i < v.getCidades().size(); ++i) {
+		for (size_t j = 0; j < tempoMax; ++j) {
+			if (i == 0) {
 				p.getGraph().getA()[i][j] = 0;
-			}
-			else{
-				if(p.getGraph().getVertexSet().at(i-1)->getInfo().getTempo() <= j){
-					int dif_tempos = j - p.getGraph().getVertexSet().at(i-1)->getInfo().getTempo();
-					if( p.getGraph().getVertexSet().at(i-1)->getInfo().getClassificacao() + p.getGraph().getA()[i-1][dif_tempos] > p.getGraph().getA()[i-1][j]){
-						p.getGraph().getA()[i][j] = p.getGraph().getVertexSet().at(i-1)->getInfo().getClassificacao() + p.getGraph().getA()[i-1][dif_tempos];
+			} else {
+				if (p.getGraph().getVertexSet().at(i - 1)->getInfo().getTempo()
+						<= j) {
+					int dif_tempos =
+							j
+									- p.getGraph().getVertexSet().at(i - 1)->getInfo().getTempo();
+					if (p.getGraph().getVertexSet().at(i - 1)->getInfo().getClassificacao()
+							+ p.getGraph().getA()[i - 1][dif_tempos]
+							> p.getGraph().getA()[i - 1][j]) {
+						p.getGraph().getA()[i][j] =
+								p.getGraph().getVertexSet().at(i - 1)->getInfo().getClassificacao()
+										+ p.getGraph().getA()[i - 1][dif_tempos];
 						// cria a mochila
+					} else {
+						p.getGraph().getA()[i][j] =
+								p.getGraph().getA()[i - 1][j];
 					}
-					else {
-						p.getGraph().getA()[i][j] = p.getGraph().getA()[i-1][j];
-					}
-				}
-				else
-				{
-					p.getGraph().getA()[i][j] = p.getGraph().getA()[i-1][j];
+				} else {
+					p.getGraph().getA()[i][j] = p.getGraph().getA()[i - 1][j];
 				}
 			}
-			pair<int,int> p;
+			pair<int, int> p;
 			p.first = i;
 			p.second = j;
 			classificacoes.push_back(p);
@@ -73,27 +77,26 @@ vector<Cidade> Knapsackproblem(Viagem v, Path p, int tempoMax){
 
 	sort(classificacoes.begin(), classificacoes.end());
 
-	iMax = classificacoes.at(classificacoes.size() -1).first;
-	jMax = classificacoes.at(classificacoes.size() -1).second;
-
+	iMax = classificacoes.at(classificacoes.size() - 1).first;
+	jMax = classificacoes.at(classificacoes.size() - 1).second;
 
 	//encontrar as cidades a serem usadas.
 
-	size_t j=jMax;
-	size_t i=iMax;
+	size_t j = jMax;
+	size_t i = iMax;
 
-	while(i>0 && j>0){
-		if(p.getGraph().getA()[i][j] != p.getGraph().getA()[i-1][j]){
-			res.push_back(p.getGraph().getVertexSet().at(i-1)->getInfo());
-			j -= p.getGraph().getVertexSet().at(i-1)->getInfo().getTempo();
+	while (i > 0 && j > 0) {
+		if (p.getGraph().getA()[i][j] != p.getGraph().getA()[i - 1][j]) {
+			res.push_back(p.getGraph().getVertexSet().at(i - 1)->getInfo());
+			j -= p.getGraph().getVertexSet().at(i - 1)->getInfo().getTempo();
 			i--;
-		}
-		else  i--;
+		} else
+			i--;
 	}
 	return res;
 }
 
-vector<Cidade> Knapsackproblemtestar(int tempoMax){
+vector<Cidade> Knapsackproblemtestar(int tempoMax) {
 
 	vector<Cidade> teste;
 
@@ -111,117 +114,109 @@ vector<Cidade> Knapsackproblemtestar(int tempoMax){
 
 	vector<Cidade> res;
 
-	int a[teste.size()+1][tempoMax+1];
+	int a[teste.size() + 1][tempoMax + 1];
 	int iMax = 0;
 	int jMax = 0;
 
-	vector<pair<int,int> > classificacoes;
+	vector<pair<int, int> > classificacoes;
 
-
-	for(size_t i=0; i < teste.size()+1 ; ++i){
-		for(size_t j=0; j < tempoMax+1 ; ++j){
-			if(i==0){
+	for (size_t i = 0; i < teste.size() + 1; ++i) {
+		for (size_t j = 0; j < tempoMax + 1; ++j) {
+			if (i == 0) {
 				a[i][j] = 0;
-			}
-			else{
-				if(teste.at(i-1).getTempo() <= j){
-					int dif_tempos = j - teste.at(i-1).getTempo();
-					if(( teste.at(i-1).getClassificacao() + a[i-1][dif_tempos] ) > a[i-1][j]){
-						a[i][j] = teste.at(i-1).getClassificacao() + a[i-1][dif_tempos];
+			} else {
+				if (teste.at(i - 1).getTempo() <= j) {
+					int dif_tempos = j - teste.at(i - 1).getTempo();
+					if ((teste.at(i - 1).getClassificacao()
+							+ a[i - 1][dif_tempos]) > a[i - 1][j]) {
+						a[i][j] = teste.at(i - 1).getClassificacao()
+								+ a[i - 1][dif_tempos];
+					} else {
+						a[i][j] = a[i - 1][j];
 					}
-					else {
-						a[i][j] = a[i-1][j];
-					}
-				}
-				else
-				{
-					a[i][j] = a[i-1][j];
+				} else {
+					a[i][j] = a[i - 1][j];
 				}
 			}
-			pair<int,int> p;
+			pair<int, int> p;
 			p.first = i;
 			p.second = j;
 			classificacoes.push_back(p);
 		}
 	}
 
-
-	for(size_t i=0; i<teste.size()+1; ++i){
-		for(size_t j=0; j<teste.size()+1; ++j){
+	for (size_t i = 0; i < teste.size() + 1; ++i) {
+		for (size_t j = 0; j < teste.size() + 1; ++j) {
 			cout << a[i][j] << " ";
 		}
 		cout << endl;
 	}
 
-
-
-
-
 	//encontrar o maximo das classificações
 
 	sort(classificacoes.begin(), classificacoes.end());
 
-	iMax = classificacoes.at(classificacoes.size()-1).first;
-	jMax = classificacoes.at(classificacoes.size()-1).second;
+	iMax = classificacoes.at(classificacoes.size() - 1).first;
+	jMax = classificacoes.at(classificacoes.size() - 1).second;
 
 	cout << iMax << endl;
 	cout << jMax << endl;
 
-
-
 	cout << res.size() << endl;
 
+	size_t j = jMax;
+	size_t i = iMax;
 
-
-	size_t j=jMax;
-	size_t i=iMax;
-
-	while(i>0 && j>0){
-		if(a[i][j] != a[i-1][j]){
-			res.push_back(teste.at(i-1));
-			j -= teste.at(i-1).getTempo();
+	while (i > 0 && j > 0) {
+		if (a[i][j] != a[i - 1][j]) {
+			res.push_back(teste.at(i - 1));
+			j -= teste.at(i - 1).getTempo();
 			i--;
-		}
-		else i--;
+		} else
+			i--;
 	}
 
+	cout
+			<< "///////////////////////////////////////////////////////////////////"
+			<< endl;
 
-	cout <<"///////////////////////////////////////////////////////////////////" << endl;
-
-	for(size_t i=0; i<res.size(); ++i){
+	for (size_t i = 0; i < res.size(); ++i) {
 		cout << res[i].getNome() << endl;
 	}
 
-	cout <<"///////////////////////////////////////////////////////////////////" << endl;
+	cout
+			<< "///////////////////////////////////////////////////////////////////"
+			<< endl;
 
 	return res;
 
 }
 
-
-int posCidade(Cidade c, Path p){
-	for(size_t i=0; i<p.getGraph().getVertexSet().size(); ++i){
-		if(p.getGraph().getVertexSet().at(i)->getInfo().getNome() == c.getNome()){
+int posCidade(Cidade c, Path p) {
+	for (size_t i = 0; i < p.getGraph().getVertexSet().size(); ++i) {
+		if (p.getGraph().getVertexSet().at(i)->getInfo().getNome()
+				== c.getNome()) {
 			return i;
 		}
 	}
 }
 
-int tempoNasCidades(vector<Cidade> cidadesAusar){
+int tempoNasCidades(vector<Cidade> cidadesAusar) {
 	int res = 0;
-	for(int i =0; i<cidadesAusar.size(); ++i){
+	for (int i = 0; i < cidadesAusar.size(); ++i) {
 		res += cidadesAusar.at(i).getTempo();
 	}
 	return res;
 }
 
-vector<Cidade> ordenaVector(vector<Cidade> cidadesAusar, Path p){
+vector<Cidade> ordenaVector(vector<Cidade> cidadesAusar, Path p) {
 
 	vector<Cidade> res;
 
-	for(size_t i=0; i<p.getGraph().getVertexSet().size(); ++i){
-		for(size_t j=0; j<cidadesAusar.size(); ++j){
-			if(cidadesAusar.at(j).getNome() == p.getGraph().getVertexSet().at(i)->getInfo().getNome()){
+	for (size_t i = 0; i < p.getGraph().getVertexSet().size(); ++i) {
+		for (size_t j = 0; j < cidadesAusar.size(); ++j) {
+			if (cidadesAusar.at(j).getNome()
+					== p.getGraph().getVertexSet().at(i)->getInfo().getNome()) {
 				res.push_back(p.getGraph().getVertexSet().at(i)->getInfo());
 			}
 		}
@@ -230,78 +225,56 @@ vector<Cidade> ordenaVector(vector<Cidade> cidadesAusar, Path p){
 	return res;
 }
 
-bool FWShortestPath(Path p, vector<Cidade> cidadesAusar, int tempoMax){
+bool FWShortestPath(Path p, vector<Cidade> cidadesAusar, int tempoMax) {
 
-	int tempos =0;
-	int melhorTempo=tempoMax;
+	int tempos = 0;
+	int melhorTempo = tempoMax;
 
 	vector<int> melhorsequencia;
 	vector<int> inteiroscidades;
 
 	p.getGraph().floydWarshallShortestPath();
 
-
 	cidadesAusar = ordenaVector(cidadesAusar, p);
 
-<<<<<<< HEAD
-=======
-	for(size_t i=0; i < cidadesAusar.size(); ++i){
+	for (size_t i = 0; i < cidadesAusar.size(); ++i) {
 		inteiroscidades.push_back(i);
-		cout << cidadesAusar[i].getNome() << endl;
 	}
 
->>>>>>> origin/master
-	while(std::next_permutation(inteiroscidades.begin(), inteiroscidades.end())){
+	while (std::next_permutation(inteiroscidades.begin(), inteiroscidades.end())) {
 
 		tempos = 0;
 		tempos += tempoNasCidades(cidadesAusar);
 
-<<<<<<< HEAD
-=======
+		for (size_t i = 1; i < inteiroscidades.size(); ++i) {
 
->>>>>>> origin/master
-		for(size_t i=1; i < inteiroscidades.size(); ++i){
+			tempos +=
+					p.getGraph().getW()[posCidade(cidadesAusar.at(i - 1), p)][posCidade(
+							cidadesAusar.at(i), p)];
 
-			tempos += p.getGraph().getW()[posCidade(cidadesAusar.at(i-1),p)][posCidade(cidadesAusar.at(i),p)];
+			if (tempos <= melhorTempo) {
 
-			if(tempos <= melhorTempo){
-<<<<<<< HEAD
-				//cout << tempos << " " <<  melhorTempo << endl;
-				//cout << "Passou no maior" << endl;
 				melhorsequencia = inteiroscidades;
 				melhorTempo = tempos;
-				//cout << "melhor sequencia\n";
-				for(size_t i=0; i<melhorsequencia.size(); i++){
-					cout <<  melhorsequencia.at(i) << endl;
-				}
-=======
-				cout << "entrou no IF" << endl;
 				melhorsequencia = inteiroscidades;
 				melhorTempo = tempos;
->>>>>>> origin/master
 			}
 
-			else{
+			else {
 
-				if(tempos > melhorTempo){
-<<<<<<< HEAD
-
-					//cout << tempos << " " <<  melhorTempo << endl;
-					//cout << "NAAAAAAAAAAAAAAAOOOOOOOOOOO" << endl;
-=======
-					cout << "entrou no ELSE" << endl;
->>>>>>> origin/master
+				if (tempos > melhorTempo) {
 					tempos = 0;
 					tempos += tempoNasCidades(cidadesAusar);
 				}
 			}
 
-
 		}
 	}
 
-
-	return  (melhorTempo + p.getGraph().getW()[posCidade(cidadesAusar.at(0),p)][posCidade(cidadesAusar.at(melhorsequencia.size()-1),p)]) <= tempoMax;
+	return (melhorTempo
+			+ p.getGraph().getW()[posCidade(cidadesAusar.at(0), p)][posCidade(
+					cidadesAusar.at(melhorsequencia.size() - 1), p)])
+			<= tempoMax;
 
 }
 
